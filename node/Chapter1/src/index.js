cebaa756bf2abd745d911540101a4b96d5f97e3c
gabrielsimongianotti <https://gabrielsimongianotti@github.com>
2021-03-14 20:06:11 -0300

    add validation  cpf in statement

--- node/Chapter1/src/index.js
+++ node/Chapter1/src/index.js
@@ -6,8 +6,8 @@ app.use(express.json());
 
 const customers = [];
 
-app.post("/account", (resquest, response) => {
-  const { cpf, name } = resquest.body;
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
 
   const cpfExistes = customers.some((customer) => customer.cpf === cpf);
   if (cpfExistes) {
@@ -23,11 +23,15 @@ app.post("/account", (resquest, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (resquest, response) => {
-  const { cpf } = resquest.params;
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
 
   const customer = customers.find((customer) => customer.cpf === cpf);
 
+  if(!customer){
+    return response.status(400).json({ error: "cpf already exists!" });
+  }
+  
   return response.json(customer.statement)
 });
 app.listen(3333);
